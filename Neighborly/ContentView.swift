6295a6950c92978311e5f925924f17ab101567a2
rudywasd <gameinluki@gmail.com>
2026-05-11 18:59:08 +0200

    Initial Commit

---
+++ Neighborly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Neighborly
+//
+//  Created by Łukasz on 11/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Neighborly/NeighborlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeighborlyApp.swift
+//  Neighborly
+//
+//  Created by Łukasz on 11/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct NeighborlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
